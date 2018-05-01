5eb8243cb9e53f1c571d24888fa4db5b75958d16
ruslanashaari <ruslan.ashaari@gmail.com>
2018-05-02 04:56:44 +0800

    laracast flash noti

--- config/app.php
+++ config/app.php
@@ -152,6 +152,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laracasts\Flash\FlashServiceProvider::class,
 
         /*
          * Application Service Providers...
